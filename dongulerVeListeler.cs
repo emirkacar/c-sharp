aed56698f14d943150c05e9d4dc2c646c8393074
emirkacar <emirkacar86@gmail.com>
2024-10-27 00:50:02 +0300

    Add files via upload

---
+++ dongulerVeListeler.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diziler konusu
+
+            /*string[] colors = new string[4];
+            colors[0] = "Kirmizi";
+            colors[1] = "Sari";
+            colors[2] = "Beyaz";
+            colors[3] = "Mavi";
+
+            Console.WriteLine(colors[0]);*/
+
+            //string[] cities = new string[10];
+            //int i;
+
+            //for (i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine("{0}.sehiri giriniz: ", i+1);
+            //    cities[i] = Console.ReadLine();
+            //}
+            //for(i=0;i<cities.Length; i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+            //int[] numbers = { 4, 10, 25, 26, 36, 29, 14, 15, 20, 30, 62, 84 };
+            //int i;
+
+            //for(i=0;i<numbers.Length; i++)
+            //{
+            //    if(numbers[i] %2==1)
+            //    {
+            //        numbers[i]++;
+            //    }
+            //}
+            //for(i=0;i<numbers.Length;i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //Dizideki en buyuk elemani bulan algoritma
+
+            //int[] dizi = { 100, 200, 300, -5, 26, 90000, 28, 13, 37 };
+            //int i;
+            //int enB = dizi[0];
+            //int enK = dizi[0];
+
+            //for(i=0;i<dizi.Length; i++)
+            //{
+            //    if (enB < dizi[i])
+            //    {
+            //        enB = dizi[i];
+            //    }
+            //    if (enK > dizi[i])
+            //    {
+            //        enK = dizi[i];
+            //    }
+            //}
+            //Console.WriteLine("Dizinin en buyuk elemani = {0},en kucuk elemani = {1}",enB,enK);
+
+
+            //int[] numbers = { 1, 2, 300, -4, 0, };
+            //Array.Sort(numbers);
+            //int i;
+
+            //for(i=0;i<numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //string[] customers = { "Emir", "Rafet" };
+
+            //int index = Array.IndexOf(customers, "Emir");
+            //Console.WriteLine(index);
+
+            //int[] numbers = { 45,85,96,74,10,25,22,36};
+
+            //Console.WriteLine("Dizinin en buyuk elemani = {0}", numbers.Max());
+
+            //Sayilari dongunun icinde alma algoritmasi.
+            //int[] sayilar = new int[5];
+            //int i=0;
+            //int toplam = 0;
+            //while(i<5)
+            //{
+            //    Console.Write($"Lutfen {i+1}. elemani giriniz.");
+            //    sayilar[i] = int.Parse(Console.ReadLine());
+            //    i++;
+            //}
+
+            //for(i=0;i<sayilar.Length;i++)
+            //{
+            //    toplam += sayilar[i];
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(toplam);
+
+            //Tek ve cift sayilari ayri ayri listeleme.
+
+            //int[] tekSayilar = new int[100];
+            //int[] ciftSayilar = new int[100];
+            //int[] sayilar = { 100, 200, 300, 400, 501, 62, 34, 78, 99, 106, 53 };
+            //int i;
+            //int ciftIndex = 0;
+            //int tekIndex = 0;
+            //for(i=0;i<sayilar.Length;i++)
+            //{
+            //    if(sayilar[i] %2==0)
+            //    {
+            //        ciftSayilar[ciftIndex] = sayilar[i];
+            //        ciftIndex++;
+            //    }
+            //    else
+            //    {
+            //        tekSayilar[tekIndex] = sayilar[i];
+            //        tekIndex++;
+            //    }
+            //}
+            //Console.WriteLine("Tek sayilar listesi:");
+            //for(i=0;i<tekIndex;i++)
+            //{
+            //    Console.WriteLine(tekSayilar[i]);
+            //}
+            //Console.WriteLine("Cift sayilar listesi:");
+            //for(i=0;i<ciftIndex;i++)
+            //{
+            //    Console.WriteLine(ciftSayilar[i]);
+            //}
+
+           
+
+
+            
+            
+                
+
+            Console.ReadLine();
+        }
+    }
+}
